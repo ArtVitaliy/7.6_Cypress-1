ea6ff3a70e904626ed06c28164aecd01186d2806
ArtVitaly <0331116@mail.ru>
2023-08-30 17:23:11 +0300

    fixed format

--- cypress2.config.js
+++ cypress2.config.js
@@ -3,10 +3,10 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {},
-    baseUrl: 'http://localhost:3000/',
+    baseUrl: "http://localhost:3000/",
     viewportWidth: 360,
     viewportHeight: 740,
     defaultCommandTimeout: 10000,
-    video: false
+    video: false,
   },
 });
